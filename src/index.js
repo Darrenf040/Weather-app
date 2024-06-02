040610263b680792deec0451fe798617925c10fb
Darrenf040 <darrenfr83@gmail.com>
2024-06-02 12:53:15 -0500

    function to get current weather

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-let hello = "dfd";
+const currentWeatherUrl =
+  "http://api.weatherapi.com/v1/current.json?key=318ddc196cd94dffaab10400243105&q=houston";
+
+async function getCurrentWeather(apiUrl) {
+  const currentWeather = await fetch(apiUrl, {
+    mode: "cors",
+  });
+  const response = await currentWeather.json();
+
+  console.log(currentWeather);
+  console.log(response);
+}
+getCurrentWeather(currentWeatherUrl);

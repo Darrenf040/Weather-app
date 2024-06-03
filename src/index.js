93e50b23861ad6b54002a306db0011313d051e65
Darrenf040 <darrenfr83@gmail.com>
2024-06-03 16:33:08 -0500

    added functions to use the json response and retrieve the data i need from them

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-async function getCurrentWeather(userLocation) {
+async function weatherLocationData(userLocation) {
   //api url changes its location based on userLocations value
   const currentWeatherUrl = `http://api.weatherapi.com/v1/current.json?key=318ddc196cd94dffaab10400243105&q=${userLocation}`;
 
@@ -10,10 +10,26 @@ async function getCurrentWeather(userLocation) {
   try {
     //wait for response to be converted to json
     const response = await currentWeather.json();
-
     console.log(response);
+    console.log(response.current.h);
+
+    return response;
   } catch (err) {
     console.error(err);
   }
 }
-getCurrentWeather("");
+weatherLocationData("houston");
+
+//returns locations temerature in fareignheight
+function getCurrentTempF(locationDataJson) {
+  return locationDataJson.current.temp_f;
+}
+
+//returns locations temerature in celsius
+function getCurrentTempC(locationDataJson) {
+  return locationDataJson.current.temp_c;
+}
+//returns locatoins wind speed in mph
+function getCurrentWindSpeed(locationDataJson) {
+  return locationDataJson.wind_mph;
+}

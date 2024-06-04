6abafa7cf43724ffc2d406910530d47782730277
Darrenf040 <darrenfr83@gmail.com>
2024-06-04 10:58:05 -0500

    get functions takes an object containgin weather data as an argument

--- src/currentWeather.js
+++ src/currentWeather.js
@@ -1,42 +1,33 @@
-export async function weatherLocationData(userLocation) {
-  //api url changes its location based on userLocations value
-  const currentWeatherUrl = `http://api.weatherapi.com/v1/current.json?key=318ddc196cd94dffaab10400243105&q=${userLocation}`;
+export default async function weatherLocationData(city) {
+  //api url changes has dynamic query parameter (city)
+  const currentWeatherUrl = `http://api.weatherapi.com/v1/current.json?key=318ddc196cd94dffaab10400243105&q=${city}`;
 
   //wait for api call to return a response
-  const currentWeather = await fetch(currentWeatherUrl, {
+  const response = await fetch(currentWeatherUrl, {
     mode: "cors",
   });
 
   try {
     //wait for response to be converted to json
-    const response = await currentWeather.json();
-    return response;
+    const weatherData = await response.json();
+    return weatherData;
   } catch (err) {
     console.error(err);
   }
 }
 
-//json with the locations data
-const weatherResponse = await weatherLocationData("London"); //adding await returns the promises value
-
 //returns locations temerature in fareignheight
-function getCurrentTempF() {
-  return weatherResponse.current.temp_f;
+function getCurrentTempF(weatherData) {
+  return Math.round(weatherData.current.temp_f);
 }
 
 //returns locations temerature in celsius
-function getCurrentTempC() {
-  return weatherResponse.current.temp_c;
+function getCurrentTempC(weatherData) {
+  return Math.round(weatherData.current.temp_c);
 }
 //returns locations wind speed in mph
-function getCurrentWindSpeed() {
-  return weatherResponse.current.wind_mph;
+function getCurrentWindSpeed(weatherData) {
+  return Math.round(weatherData.current.wind_mph);
 }
 
-function printLocationData() {
-  console.log(getCurrentTempF());
-  console.log(getCurrentTempC());
-  console.log(getCurrentWindSpeed());
-}
-
-printLocationData();
+export { getCurrentTempF, getCurrentTempC, getCurrentWindSpeed };

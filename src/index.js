bab02e89fd7173045a992a7ff6b2453429d23326
Darrenf040 <darrenfr83@gmail.com>
2024-06-04 11:17:45 -0500

    event listener to wait for user input and use that input to pass in to the api request

--- src/index.js
+++ src/index.js
@@ -1,2 +1,30 @@
-import { weatherLocationData } from "./currentWeather.js";
-import "./forecast.js";
+import weatherLocationData, {
+  getCurrentTempF,
+  getCurrentTempC,
+  getCurrentWindSpeed,
+} from "./currentWeather.js";
+import forecastLocationData, {
+  getHighTemp,
+  getLowTemp,
+  getAvgTemp,
+} from "./forecast.js";
+
+const citySearchBox = document.getElementById("city");
+const confirmSearch = document.querySelector(".search-city");
+
+confirmSearch.addEventListener("click", async (e) => {
+  e.preventDefault();
+  //users input value is the city they typed in search box
+  const userInput = citySearchBox.value;
+  //pass in the users input and wait to retrieve the citys data in an object
+  const locationData = await weatherLocationData(userInput);
+  console.log(getCurrentTempF(locationData));
+  console.log(getCurrentTempC(locationData));
+  console.log(getCurrentWindSpeed(locationData));
+
+  //pass in users input and wait to retrieve citys forecast data in an object
+  const forecastData = await forecastLocationData(userInput);
+  console.log(getHighTemp(forecastData));
+  console.log(getLowTemp(forecastData));
+  console.log(getAvgTemp(forecastData));
+});
